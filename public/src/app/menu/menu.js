936412973f8dab8c73d3929e91c5534e196db3d2
Andrew Grzeszczak <andrew.s.grzeszczak@gmail.com>
2013-08-30 11:39:04 -0500

    Added menu.

---
+++ public/src/app/menu/menu.js
@@ -0,0 +1,14 @@
+angular.module( 'ngBoilerplate.menu', [
+  'ui.state',
+  'placeholders',
+  'ui.bootstrap',
+  'titleService'
+])
+
+.controller( 'MenuCtrl', function MenuCtrl($scope) {
+    $scope.menu = [
+      { title : "Home", link: "#/home"}  
+    ];
+})
+
+;
